1bcbae43e985a95c602e4483c6edaae533368cee
Phil Nash <philnash@gmail.com>
2017-08-14 17:05:58 +0100

    Adds routes for phone verifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :phone_verifications, :only => [:new, :create] do |p|
+    collection do
+      get 'challenge'
+      post 'verify'
+      get 'success'
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
