75da0efe9529300e8553a668c6bb62a278c9eea5
Phil Nash <philnash@gmail.com>
2017-08-14 19:13:52 +0100

    Plain HTML forms wired up for verification

--- app/controllers/phone_verifications_controller.rb
+++ app/controllers/phone_verifications_controller.rb
@@ -1,16 +1,23 @@
 class PhoneVerificationsController < ApplicationController
-  def new
-  end
-
   def create
-  end
-
-  def challenge
+    session[:phone_number] = params[:phone_number]
+    session[:country_code] = params[:country_code]
+    @response = Authy::PhoneVerification.start(via: params[:method], country_code: params[:country_code], phone_number: params[:phone_number])
+    if @response.ok?
+      redirect_to challenge_phone_verifications_path
+    else
+      render :new
+    end
   end
 
   def verify
-  end
-
-  def success
+    @response = Authy::PhoneVerification.check(verification_code: params[:code], country_code: session[:country_code], phone_number: session[:phone_number])
+    if @response.ok?
+      session[:phone_number] = nil
+      session[:country_code] = nil
+      redirect_to success_phone_verifications_path
+    else
+      render :challenge
+    end
   end
 end
